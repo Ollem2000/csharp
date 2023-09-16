d5ede6987f92c56aa56d8ad14ed5b55a3b4e99b5
Allan Oliveira <allanolme@gmail.com>
2023-09-16 18:02:55 -0300

    Create int_parse.cs

---
+++ int_parse.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int num;
+	
+	Console.WriteLine ("Hello World");
+	
+	num = int.Parse(Console.ReadLine());
+	Console.WriteLine(num);
+  }
+}
