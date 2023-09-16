9ead5dec7adaf53032b01a4c0761251af945e7e4
Allan Oliveira <allanolme@gmail.com>
2023-09-16 19:11:44 -0300

    Update int_parse.cs

--- int_parse.cs
+++ int_parse.cs
@@ -3,9 +3,6 @@ using System;
 class Program {
   public static void Main (string[] args) {
     int num;
-	
-	Console.WriteLine ("Hello World");
-	
 	num = int.Parse(Console.ReadLine());
 	Console.WriteLine(num);
   }
