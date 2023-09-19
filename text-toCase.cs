8c0d2a39358e7b2f3008213dda693487abe493ca
Allan Oliveira <allanolme@gmail.com>
2023-09-19 20:45:53 -0300

    Create text-toCase.cs

---
+++ text-toCase.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program{
+	public static void Main(string[] args){
+		String text = "eSsE É meU TeXto";
+		Console.WriteLine(text.ToUpper());
+		Console.WriteLine(text.ToLower());
+	}
+}
