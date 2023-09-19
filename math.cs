85198acceafad55ad798986ab6316c4960665ea5
Allan Oliveira <allanolme@gmail.com>
2023-09-19 20:39:53 -0300

    Create math.cs

---
+++ math.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program{
+	public static void Main(string[] args){
+		Console.WriteLine(Math.PI);
+		Console.WriteLine(Math.Pow(2, 2));
+		Console.WriteLine(Math.Sqrt(2));
+	}
+}
